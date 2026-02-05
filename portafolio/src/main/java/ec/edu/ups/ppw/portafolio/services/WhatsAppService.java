237f72b6202f3abcb0ed600c194bc326a349622d
Redrovan <robinsonredrovan@gmail.com>
2026-02-05 18:40:00 -0500

    Agrega .gitignore y elimina credenciales sensibles

--- portafolio/src/main/java/ec/edu/ups/ppw/portafolio/services/WhatsAppService.java
+++ portafolio/src/main/java/ec/edu/ups/ppw/portafolio/services/WhatsAppService.java
@@ -9,9 +9,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class WhatsAppService {
 
-    private static final String ACCOUNT_SID = "AC462a36bdd0b105477c10d66e5fbaa0f8";
-    private static final String AUTH_TOKEN = "9b0a0a6d53a3894da689265fa99cc078";
-
+   
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
